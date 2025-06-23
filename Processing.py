6a45ba0f275a50e31d6e7a920154a2867aa80a57
humaspasta <97994370+humaspasta@users.noreply.github.com>
2025-06-22 21:54:04 -0600

    Added .env file, .gitignore, and constructor to Processing

--- Processing.py
+++ Processing.py
@@ -1,39 +1,29 @@
 import pandas as pd
 import json
-
+import sqlite3 as sql
+import dotenv as env
+import os
 class Processing:
-    def scrape(filename:str):
+
+    def __init__(self , data_path):
+        self.conn = sql.connect('')
+        env.load_dotenv('info.env') #loading .env file
+
+        self.data = pd.read_csv(data_path)
+
+    def scrape(self, link:str):
         '''
         Gets a JSON file containing information of a given startup for caching.
-        Uses perplexity SONAR for retrieving website information.
+        Uses perplexity SONAR for retrieving website information. Stores information in sql database
         '''
 
-        '''
-        
-        
-        '''
-
-        info_dict = {}
-
-        startups_df = None
-        try:
-            csv = pd.read_csv(filename)
-        except FileNotFoundError as fl:
-            print("File " + filename +" not found. Check if file exists or if name is correct.")
-            return
-        except Exception as e:
-            print("Something else went wrong: " + e)
-            return
-
-        rows = startups_df['homepage_url']
-
-        for link in rows:
-            #code for retrieving JSON data for each file
-            output = ...
-            json_object = json.dumps(output)
+        output = ...
+        json_text = json.dumps(output)
+        self.store_JSON(json_text)
+      
 
           
-        
+    
     def store_JSON(filename:str):
         '''
         Stores the JSON file in a sqlite database
