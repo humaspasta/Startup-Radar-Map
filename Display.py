6c1168a81d18653824f96d55fd8dcd253271d56e
humaspasta <humzap20@gmail.com>
2025-06-29 13:06:05 -0600

    created sample class

---
+++ Display.py
@@ -0,0 +1,37 @@
+class Display_Data:
+
+    def __init__(self):
+        pass
+
+    
+    def project_vector_2d(self , vector):
+        '''
+        Responsible for projecting a given vector to 2d
+        '''
+        pass
+
+    
+    def create_hovercard(self):
+        pass
+
+    def plot_all_vectors(self):
+        '''
+        Responsible for plotting all 2d vectors. 
+        '''
+        pass
+
+    def lasso(self):
+        '''
+        Retrieves select points in the lasso
+        '''
+        pass
+
+    def export_lasso(self):
+        '''
+        Exports the set of points retrieved in the lasso. 
+        '''
+        pass
+
+    def update_log(self):
+        pass
+
